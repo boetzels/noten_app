f2793ede44babd08e45f9fc14eeff80d229939e4
boetzels <71010527+boetzels@users.noreply.github.com>
2022-01-29 09:38:51 +0100

    Update Notenskala.kt
    make class methods private

--- app/src/main/java/com/boetzel/notenskala/Notenskala.kt
+++ app/src/main/java/com/boetzel/notenskala/Notenskala.kt
@@ -19,8 +19,8 @@ class NotenSkala(var maxPoints: Double = 20.0, var suffPoints: Double = 12.0, va
     }
 
     // main function
-    fun calcGrade(): Double {
-        Log.d(LOGTAG, "entering private fun calcGrade($maxPoints, $suffPoints, $myPoints, $pointPath, $gradeRound)")
+    public fun calcGrade(): Double {
+        Log.d(LOGTAG, "entering public fun calcGrade($maxPoints, $suffPoints, $myPoints, $pointPath, $gradeRound)")
         //val pointDecs : Byte =
         val pointDecimals = this.getNbrOfDecimals(pointPath)
         val gradeDecimals = this.getNbrOfDecimals(gradeRound)
@@ -36,12 +36,12 @@ class NotenSkala(var maxPoints: Double = 20.0, var suffPoints: Double = 12.0, va
 
         val grade = Math.round(Math.round((myPoints*linFunc["m"]!!.toDouble()+linFunc["b"]!!.toDouble())/gradeRound)*gradeRound*Math.pow(10.0, gradeDecimals.toDouble()))/Math.pow(10.0, gradeDecimals.toDouble()).toDouble()
 
-        Log.d(LOGTAG, "leaving private fun calcGrade($grade)")
+        Log.d(LOGTAG, "leaving public fun calcGrade($grade)")
         return grade
     }
 
     // get number of decimals of given number
-    fun getNbrOfDecimals(number : Double) : Byte {
+    private fun getNbrOfDecimals(number : Double) : Byte {
         Log.d(LOGTAG, "entering private fun getNbrOfDecimals($number)")
         if (Math.floor(number) == number) {
             return 0
@@ -59,7 +59,7 @@ class NotenSkala(var maxPoints: Double = 20.0, var suffPoints: Double = 12.0, va
     }
 
     // calcs linear function of 2 points
-    fun calcLinearFunction(p1x : Double, p1y : Double, p2x : Double, p2y : Double) : Map<String,Double>  {
+    private fun calcLinearFunction(p1x : Double, p1y : Double, p2x : Double, p2y : Double) : Map<String,Double>  {
         Log.d(LOGTAG, "entering private fun calcLinearFunction($p1x, $p1y, $p2x, $p2y)")
         val m: Double = (p2y - p1y) / (p2x - p1x)
         val b: Double = p1y - m * p1x
